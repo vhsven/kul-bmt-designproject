ace758e6bb5ed48428a09f57cf6fb92a72fcd24e
mythicfreak <mythic.freak@gmail.com>
2014-04-09 22:53:29 +0200

    Mask post-processing

--- NoduleDetection/src/FixedThresholdTest.py
+++ NoduleDetection/src/FixedThresholdTest.py
@@ -2,6 +2,8 @@ import pylab
 from matplotlib.widgets import Slider
 import numpy as np
 import numpy.ma as ma
+from scipy import ndimage
+from skimage.morphology import reconstruction
 from DicomFolderReader import DicomFolderReader 
 
 
@@ -17,20 +19,31 @@ def calcThresholds(mySlice, low, high):
     #print("Slice {}: rescaled grey levels {} -- {}".format(mySlice, minI, maxI))
     #p = getHistogram(HU, minI, maxI)
     
-    masked = ma.masked_outside(HU, low, high)
+    masked = ma.masked_inside(HU, low, high)
+    
+    #opening
+    newmask = ndimage.binary_opening(masked.mask, structure=np.ones((9,9)), iterations=1).astype(np.int)
+    #newmask = ndimage.binary_dilation(newmask, structure=np.ones((9,9)), iterations=1).astype(np.int)
+    #masked.mask = newmask 
+    #reconstruction = ndimage.binary_propagation(eroded_square, mask=square)
+
+    seed = np.copy(newmask)
+    seed[0:-1, 0:-1] = newmask.max()
+    newmask = reconstruction(seed, newmask, method='erosion')
+
     minI = masked.min() # find the new minimum inside mask region
     maxI = masked.max()
     p = getHistogram(masked, minI, maxI)
     barX = np.arange(minI, maxI, BIN_SIZE)
     
-    return masked, p, barX
+    return masked, p, barX, newmask
     
 BIN_SIZE = 10
 LOW_INIT = 1600
 HIGH_INIT = 4000
 
-myPath = "../data/LIDC-IDRI/LIDC-IDRI-0001/1.3.6.1.4.1.14519.5.2.1.6279.6001.298806137288633453246975630178/000000"
-#myPath = "../data/LIDC-IDRI/LIDC-IDRI-0002/1.3.6.1.4.1.14519.5.2.1.6279.6001.490157381160200744295382098329/000000"
+#myPath = "../data/LIDC-IDRI/LIDC-IDRI-0001/1.3.6.1.4.1.14519.5.2.1.6279.6001.298806137288633453246975630178/000000"
+myPath = "../data/LIDC-IDRI/LIDC-IDRI-0002/1.3.6.1.4.1.14519.5.2.1.6279.6001.490157381160200744295382098329/000000"
 dfr = DicomFolderReader(myPath)
 
 #pylab.ion() #interactive mode: don't stop on show()
@@ -38,13 +51,10 @@ dfr = DicomFolderReader(myPath)
 fig, ax = pylab.subplots()
 pylab.subplots_adjust(bottom=0.25)
 
-masked, p, barX = calcThresholds(0, LOW_INIT, HIGH_INIT)
-sp1 = pylab.subplot(121)
-pylab.title("Slice {}".format(0))
-pylab.imshow(masked.mask, cmap=pylab.gray())
-sp2 = pylab.subplot(122)
-pylab.title("Histogram")
-pylab.bar(barX, p, 0.35)
+#masked, p, barX, mask2 = calcThresholds(0, LOW_INIT, HIGH_INIT)
+sp1 = pylab.subplot(131)
+sp2 = pylab.subplot(132)
+sp3 = pylab.subplot(133)
     
 #axes: left, bottom, width, height
 sSlider = Slider(pylab.axes([0.1, 0.15, 0.8, 0.03]), 'Slice', 0, dfr.getNbSlices()-1, 0, valfmt='%1.0f')
@@ -56,12 +66,14 @@ def update(val):
     low = int(minSlider.val)
     high = int(maxSlider.val)
     
-    masked, p, barX = calcThresholds(mySlice, low, high)
+    masked, p, barX, mask2 = calcThresholds(mySlice, low, high)
     sp1.clear()
     sp2.clear()
+    sp3.clear()
     
     sp1.imshow(masked.mask, cmap=pylab.gray())
     sp2.bar(barX, p, 0.35)
+    sp3.imshow(mask2, cmap=pylab.gray())
     
     fig.canvas.draw_idle()
 
@@ -69,6 +81,7 @@ sSlider.on_changed(update)
 minSlider.on_changed(update)
 maxSlider.on_changed(update)
 
+update(0)
 pylab.show()
 
 #Idee 1:

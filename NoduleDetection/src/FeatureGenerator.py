e2271f452666f9188e199f2de0cb1ccc4789720e
s0190440 <s0190440@192.168.0.110>
2014-05-07 00:57:52 +0200

    Sigma=~15 testen?

--- NoduleDetection/src/FeatureGenerator.py
+++ NoduleDetection/src/FeatureGenerator.py
@@ -54,7 +54,7 @@ class FeatureGenerator:
             return self.getIntensityByMask(mask3D)
         if level == 2:
             N = mask3D.sum()
-            start,stop = 2,10
+            start,stop = 2,10 #TODO roughly 15 seems promising
             result = np.empty((N,stop-start+1))
             for sigma in np.arange(start,stop):
                 sigmas = np.array([sigma]*3) / np.array(self.VoxelShape)
